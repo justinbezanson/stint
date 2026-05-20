151af5eccc82bff7a15797b2ed97da4933f73eb7
Justin Bezanson <justinbezanson@gmail.com>
2026-05-19 22:00:42 -0300

    calendar table, scrolling needs work

--- resources/js/components/ReadingLogCalendarView.vue
+++ resources/js/components/ReadingLogCalendarView.vue
@@ -1,79 +1,102 @@
 <template>
-    <Table class="w-full border-t-1">
-        <TableHeader>
-            <TableRow>
-                <TableHead v-for="(day, index) in daysOfTheWeek" :key="day" :class="{ 'border-l-1': index > 0 }">
-                    {{ day }}
-                </TableHead>
-            </TableRow>
+    <div class="min-w-[965px]">
+        <Table class="border-t-1 table-fixed">
+            <TableHeader>
+                <TableRow>
+                    <TableHead v-for="(day, index) in daysOfTheWeek" :key="day" :class="{ 'border-l-1': index > 0 }">
+                        {{ day }}
+                    </TableHead>
+                </TableRow>
+            </TableHeader>
             <TableBody>
-                
+                <TableRow v-for="week in weekRows" :key="week.week">
+                    <TableCell 
+                        v-for="(day, dayIndex) in week.days" 
+                        :key="day.date" 
+                        :class="{ 'border-l-1': dayIndex > 0 }"
+                    >
+                        <div class="min-h-28">
+                            {{ day.date }}
+                        </div>
+                    </TableCell>
+                </TableRow>                
             </TableBody>
-        </TableHeader>
-    </Table>
+        </Table>
+    </div>
 </template>
 
 <script setup lang="ts">
 import Table from '@/components/ui/table/Table.vue';
-//import type { ReadingLogMonth, ReadingLogWeek } from '@/types';
+import type { ReadingLogMonth, ReadingLogWeek } from '@/types';
 import TableHead from './ui/table/TableHead.vue';
 import TableHeader from './ui/table/TableHeader.vue';
 import TableRow from './ui/table/TableRow.vue';
+import TableBody from './ui/table/TableBody.vue';
+import TableCell from './ui/table/TableCell.vue';
 
-// const props = defineProps<{
-//     currentMonth: ReadingLogMonth;
-// }>();
 
-// const previousMonth: ReadingLogMonth = {
-//     month: props.currentMonth.month === 0 ? 11 : props.currentMonth.month - 1,
-//     year: props.currentMonth.month === 0 ? props.currentMonth.year - 1 : props.currentMonth.year,
-// };
-// const nextMonth: ReadingLogMonth = {
-//     month: props.currentMonth.month === 11 ? 0 : props.currentMonth.month + 1,
-//     year: props.currentMonth.month === 11 ? props.currentMonth.year + 1 : props.currentMonth.year,
-// };
+const props = defineProps<{
+    currentMonth: ReadingLogMonth;
+}>();
+
+const previousMonth: ReadingLogMonth = {
+    month: props.currentMonth.month === 0 ? 11 : props.currentMonth.month - 1,
+    year: props.currentMonth.month === 0 ? props.currentMonth.year - 1 : props.currentMonth.year,
+};
 
 const daysOfTheWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-// let dayOfMonth = 0;
-// let maxDayOfCurrentMonth = new Date(props.currentMonth.year, props.currentMonth.month + 1, 0).getDate();
-// let maxDayOfPreviousMonth = new Date(previousMonth.year, previousMonth.month + 1, 0).getDate();
+let maxDayOfPreviousMonth = new Date(previousMonth.year, previousMonth.month + 1, 0).getDate();
 
-//const buildRows = (): ReadingLogWeek[] => {
-    //const weeks: ReadingLogWeek[] = [];
-    // let week: number = 0;
-    // let day;
+const buildRows = (): ReadingLogWeek[] => {
+    const weeks: ReadingLogWeek[] = [];
+    let day;
 
-    // while((day = incrementDayOfTheMonth()) !== false) {
-    //     let date = new Date(props.currentMonth.year, props.currentMonth.month, day as number);
-    //     let dayOfWeek = date.getDay();
-    //     let weekOfTheyear = Math.ceil((date.getDate() + 6 - dayOfWeek) / 7);
-    //     let diff = 7 - dayOfWeek;
+    let date = new Date(props.currentMonth.year, props.currentMonth.month, 1);
+    let daysInMonth = new Date(props.currentMonth.year, props.currentMonth.month + 1, 0).getDate();
+    let firstDay = date.getDay();
+    let week: ReadingLogWeek = {
+        week: 1,
+        year: props.currentMonth.year,
+        days: []
+    }
 
-    //     while(dayOfWeek < 7) {}
-    //         let week: ReadingLogWeek = {
-    //             week: weekOfTheyear,
-    //             year: props.currentMonth.year,
-    //             days: [],
-    //         }
-    //     }
+    for(let i = 0; i < firstDay; i++) {
+        week.days.push({
+            date: maxDayOfPreviousMonth - firstDay + i + 1,
+            entries: [],
+        });
+    }
 
-    //     //weeks.push(week);
-    // }
+    for(let day = 1; day <= daysInMonth; day++) {
+        week.days.push({
+            date: day,
+            entries: [],
+        });
 
-    //return weeks;
-//};
+        if (week.days.length === 7) {
+            weeks.push(week);
+            week = {
+                week: week.week + 1,
+                year: props.currentMonth.year,
+                days: [],
+            }
+        }
+    }
 
-// const incrementDayOfTheMonth: () => number | boolean = () => {
-//     dayOfMonth++;
+    let remainingDays = 7 - week.days.length;
+    for(let i = 0; i < remainingDays; i++) {
+        week.days.push({
+            date: i + 1,
+            entries: [],
+        });
+    }
 
-//     if (dayOfMonth > maxDayOfCurrentMonth) {
-//         return false;
-//     }
+    weeks.push(week);
 
-//     return dayOfMonth;
-// };
+    return weeks;
+};
 
-//const weekRows: ReadingLogWeek[] = buildRows();
+const weekRows: ReadingLogWeek[] = buildRows();
 </script>
 
 <style scoped>
