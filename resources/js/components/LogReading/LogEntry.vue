93c0da46e4c08bbed2f56316122d5bca3eae17e3
Justin Bezanson <justinbezanson@gmail.com>
2026-06-18 21:03:59 -0300

    layout work

--- resources/js/components/LogReading/LogEntry.vue
+++ resources/js/components/LogReading/LogEntry.vue
@@ -9,14 +9,54 @@
                         alt="Book Cover"
                         class="w-25 mx-auto md:mx-0"
                     >
-                    <div>
-                        info
+                    <div class="pl-4">
+                        <div class="font-bold">The Wheel Of Time</div>
+                        <div class="text-sm text-gray-500">Robert Jordan</div>
                     </div>
                 </div>
             </div>
             
             <div class="w-3/5 rounded-xl bg-gray-100 p-4 text-left">
-                log settings
+                <div class="flex gap-4"> 
+                    <div class="w-3/4">
+                        <div>Log reading for:</div>
+                    </div>
+                    <div class="w-1/4">
+                        <div>Some data here</div>
+                    </div>
+                </div>
+
+                <div class="flex gap-4"> 
+                    <div class="w-3/4">
+                        <div>Time spent reading:</div>
+                    </div>
+                    <div class="w-1/4">
+                        <Button variant="default" class="bg-rose-200 hover:bg-rose-300 text-lg text-rose-700">
+                            <Timer />
+                            Start Timer
+                        </Button>
+                    </div>
+                </div>
+
+                <div class="flex gap-4"> 
+                    <div class="w-3/4">
+                        <div>&nbsp;</div>
+                    </div>
+                    <div class="w-1/4">
+                        <div>Some data here</div>
+                    </div>
+                </div>
+
+                <div class="flex gap-4"> 
+                    <div class="w-3/4">
+                        <div>&nbsp;</div>
+                    </div>
+                    <div class="w-1/4">
+                        <Button variant="default" class="bg-rose-500 hover:bg-rose-700 text-lg">
+                            Log Reading
+                        </Button>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -24,7 +64,9 @@
 </template>
 
 <script setup lang="ts">
+import { Timer } from 'lucide-vue-next'
 import type { BookSearchResult } from '@/types/reading-log';
+import Button from '../ui/button/Button.vue';
 
 defineProps<{
     book: BookSearchResult | null;
