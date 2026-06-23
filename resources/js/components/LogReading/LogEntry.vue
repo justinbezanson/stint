2e66ea119e96b91023bb1f381ab6b56adc061898
Justin Bezanson <justinbezanson@gmail.com>
2026-06-22 21:11:42 -0300

    lauout adjustments and datepicker

--- resources/js/components/LogReading/LogEntry.vue
+++ resources/js/components/LogReading/LogEntry.vue
@@ -17,28 +17,51 @@
             </div>
             
             <div class="w-3/5 rounded-xl bg-gray-100 p-4 text-left">
-                <div class="flex gap-4"> 
+                <div class="flex gap-4 border-b border-gray-200 pb-6 mb-8"> 
                     <div class="w-3/4">
-                        <div>Log reading for:</div>
+                        <div class="font-bold">Log reading for:</div>
                     </div>
-                    <div class="w-1/4">
-                        <div>Some data here</div>
+                    <div class="w-1/4 flex justify-end">
+                        <div>
+                            <Popover>
+                                <PopoverTrigger as-child>
+                                <Button
+                                    variant="outline"
+                                    :class="cn(
+                                    'w-70 justify-start text-left font-normal',
+                                    !date && 'text-muted-foreground',
+                                    )"
+                                >
+                                    <CalendarIcon class="mr-2 h-4 w-4" />
+                                    {{ date ? formatter.format(date.toDate(getLocalTimeZone())) : "Pick a date" }}
+                                </Button>
+                                </PopoverTrigger>
+                                <PopoverContent class="w-auto p-0">
+                                <Calendar
+                                    v-model="date"
+                                    :initial-focus="true"
+                                    :default-placeholder="defaultPlaceholder"
+                                    layout="month-and-year"
+                                />
+                                </PopoverContent>
+                            </Popover>
+                        </div>
                     </div>
                 </div>
 
-                <div class="flex gap-4"> 
+                <div class="flex gap-4 items-start"> 
                     <div class="w-3/4">
-                        <div>Time spent reading:</div>
+                        <div class="font-bold">Time spent reading:</div>
                     </div>
-                    <div class="w-1/4">
-                        <Button variant="default" class="bg-rose-200 hover:bg-rose-300 text-lg text-rose-700">
+                    <div class="w-1/4 flex justify-end">
+                        <Button variant="default" class="bg-rose-200 hover:bg-rose-300 text-lg text-rose-700 cursor-pointer">
                             <Timer />
                             Start Timer
                         </Button>
                     </div>
                 </div>
 
-                <div class="flex gap-4"> 
+                <div class="flex gap-4 border-b border-gray-200 pb-6 mb-8"> 
                     <div class="w-3/4">
                         <div>&nbsp;</div>
                     </div>
@@ -51,8 +74,8 @@
                     <div class="w-3/4">
                         <div>&nbsp;</div>
                     </div>
-                    <div class="w-1/4">
-                        <Button variant="default" class="bg-rose-500 hover:bg-rose-700 text-lg">
+                    <div class="w-1/4 flex justify-end">
+                        <Button variant="default" class="bg-rose-500 hover:bg-rose-700 text-lg cursor-pointer">
                             Log Reading
                         </Button>
                     </div>
@@ -64,11 +87,25 @@
 </template>
 
 <script setup lang="ts">
-import { Timer } from 'lucide-vue-next'
+import { DateFormatter, getLocalTimeZone, today  } from '@internationalized/date'
+import type {CalendarDate} from '@internationalized/date';
+import { Timer, CalendarIcon } from 'lucide-vue-next'
+import { ref } from 'vue';
+import { Calendar } from '@/components/ui/calendar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
 import type { BookSearchResult } from '@/types/reading-log';
 import Button from '../ui/button/Button.vue';
 
 defineProps<{
     book: BookSearchResult | null;
 }>();
+
+const date = ref<CalendarDate>()
+const formatter = new DateFormatter('en-US', { dateStyle: 'full' })
+const defaultPlaceholder = today(getLocalTimeZone())
 </script>
