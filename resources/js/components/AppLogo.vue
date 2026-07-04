503486739482688c4ecce53d6a04ba24bedde886
Justin Bezanson <justinbezanson@gmail.com>
2026-07-04 16:29:23 -0300

    Add dark and light logo images; update AppLogo component to use new images

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
+
 </script>
 
 <template>
-    <div
-        class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground"
-    >
-        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
-    </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate leading-tight font-semibold"
-            >Laravel Starter Kit</span
-        >
+        <img src="/images/stint-logo-light.jpg" alt="App Logo" width="75" class="block dark:hidden" />
+        <img src="/images/stint-logo-dark.jpg" alt="App Logo" width="75" class="hidden dark:block" />
     </div>
 </template>
